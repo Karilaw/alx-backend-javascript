707bcd163bb9f0209d2611d0c9a516bd23c498f2
Karilaw <lawrencekariithi6@gmail.com>
2024-01-03 19:38:49 +0000

    second commit to test

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,4 +1,12 @@
-function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-    });
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Simulate an API call that takes 2 seconds to complete
+    setTimeout(() => {
+      // Replace this with your actual API call logic
+      const apiResponse = { data: "This is the response from the API" };
+
+      // Resolve the Promise with the API response
+      resolve(apiResponse);
+    }, 2000);
+  });
 }
